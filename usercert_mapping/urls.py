13d26c7f2dcf31289cf076b2886b2c0c93385598
Shaun Brady <shaun.brady@nimbisservices.com>
2015-02-13 13:06:47 -0500

    Adding login_required decorator

--- usercert_mapping/urls.py
+++ usercert_mapping/urls.py
@@ -2,6 +2,7 @@
 from __future__ import absolute_import
 
 from django.conf.urls import patterns, url
+from django.contrib.auth.decorators import login_required
 
 from .views import CertAuthView, CertCreateView, CertListView
 
