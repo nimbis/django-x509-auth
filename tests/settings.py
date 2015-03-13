9fc90a7d3b854e925c81c4feedeb10d4b8361b1b
Shaun Brady <shaun.brady@nimbisservices.com>
2015-03-13 19:12:16 -0400

    Testing access request with real Django (tests fail)

--- tests/settings.py
+++ tests/settings.py
@@ -94,6 +94,7 @@ TEMPLATE_CONTEXT_PROCESSORS = (
     'django.core.context_processors.request',
     'django.core.context_processors.media',
     'django.core.context_processors.static',
+    'django.core.context_processors.request',
 )
 
 MIDDLEWARE_CLASSES = (
--- x509_auth/templatetags/x509_auth_extras.py
+++ x509_auth/templatetags/x509_auth_extras.py
@@ -1,1 +1,14 @@
+# -- code: utf-8 --
+from __future__ import absolute_import
 
+from django import template
+
+from x509_auth.auth_backend import is_X509_authed
+
+register = template.Library()
+
+
+@register.assignment_tag(takes_context=True)
+def is_X509_authed(context):
+    request = context['request']
+    return is_X509_authed(request)
