c7ce7368bc5e665b9e3f3e81c8711b1982677508
Shaun Brady <shaun.brady@nimbisservices.com>
2015-03-12 23:35:45 -0400

    Testing is_X509_authed

--- x509_auth/auth_backend.py
+++ x509_auth/auth_backend.py
@@ -1,7 +1,13 @@
 # -- code: utf-8 --
 from __future__ import absolute_import
 
+from functools import wraps
+
 from django.contrib.auth.backends import ModelBackend
+from django.contrib.auth.decorators import user_passes_test
+from django.utils.decorators import available_attrs
+from django.core.urlresolvers import reverse
+from django.contrib.auth.views import redirect_to_login
 
 from .models import X509UserMapping
 
@@ -25,10 +31,31 @@ class AuthenticationBackend(ModelBackend):
             return None
 
 
-def is_X509_authed(user):
+def is_X509_authed(request):
     """
     Check how this user authenticated (did they use our backend?)
     """
+    if not request.user.is_authenticated():
+        return False
+    try:
+        if not (request.session['_auth_user_backend'] ==
+            'x509_auth.auth_backend.AuthenticationBackend'):
+            return False
+    except KeyError:
+        # Odd.. we're authed with out a backend.
+        return False
 
-    return (hasattr(user, 'backend') and
-            user.backend == 'x509_auth.auth_backend.AuthenticationBackend')
+    return True
+
+def X509_required(view_func):
+    """
+    Decorator to force that a user has authenticated with our backend.
+    Redirects them to our auth page if not.
+    """
+
+    @wraps(view_func, assigned=available_attrs(view_func))
+    def _wrapped_view(request, *args, **kwargs):
+        if not is_X509_authed(request):
+            redirect_to_login(request.get_full_path(),reverse('auth'))
+        return view_func(request, *args, **kwargs)
+    return _wrapped_view
