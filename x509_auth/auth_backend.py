f73af3912a2b8f6f00d913757478c8bc3cbf6717
Shaun Brady <shaun.brady@nimbisservices.com>
2015-03-16 20:42:17 -0400

    Return this response instead of ya know, just dropping it on the floor

--- x509_auth/auth_backend.py
+++ x509_auth/auth_backend.py
@@ -56,6 +56,6 @@ def X509_required(view_func):
     @wraps(view_func, assigned=available_attrs(view_func))
     def _wrapped_view(request, *args, **kwargs):
         if not is_X509_authed(request):
-            redirect_to_login(request.get_full_path(), reverse('auth'))
+            return redirect_to_login(request.get_full_path(), reverse('auth'))
         return view_func(request, *args, **kwargs)
     return _wrapped_view
