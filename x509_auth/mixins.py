8aaa58cfb73119816518184bd6d7e4c35a73f1ae
Shaun Brady <shaun.brady@nimbisservices.com>
2016-11-21 15:37:32 -0500

    Modify X509ContextMixin to be used in CBV or FBV
    Moves most of the guts out of get_context_data (which is a CBV method)
    out to x509_modify_context.  We parameterize all of this bits formally
    relied on by self, so that a FBV could call this the new
    x509_modify_context directly.

--- x509_auth/mixins.py
+++ x509_auth/mixins.py
@@ -5,16 +5,24 @@ from .models import X509UserMapping
 
 
 class X509ContextMixin(object):
-    def get_context_data(self, **kwargs):
-        context = super(X509ContextMixin, self).get_context_data(**kwargs)
-        if (('HTTP_X_SSL_USER_DN' in self.request.META) and
-           ('HTTP_X_SSL_AUTHENTICATED' in self.request.META)):
+
+    def x509_modify_context(self, context, request, user):
+        """
+        Some what of a utility method.  Designed to be called by FBVs.  Does
+        not rely on 'self'.  Modifies context in place.
+        """
+        if (('HTTP_X_SSL_USER_DN' in request.META) and
+           ('HTTP_X_SSL_AUTHENTICATED' in request.META)):
             # A key is asserted, see if we already have it
             certs = X509UserMapping.objects.filter(
-                user=self.request.user,
-                cert_dn=self.request.META['HTTP_X_SSL_USER_DN'])
+                user=user,
+                cert_dn=request.META['HTTP_X_SSL_USER_DN'])
             if not certs.exists():
                 for k in ['HTTP_X_SSL_USER_DN', 'HTTP_X_SSL_AUTHENTICATED']:
                     # Put in the context for possible addition
-                    context[k] = self.request.META[k]
+                    context[k] = request.META[k]
+
+    def get_context_data(self, **kwargs):
+        context = super(X509ContextMixin, self).get_context_data(**kwargs)
+        self.x509_modify_context(context, self.request, self.request.user)
         return context
